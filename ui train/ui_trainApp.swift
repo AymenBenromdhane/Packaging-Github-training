ddb4ab307d5bbd0b53eeec35d5eb3ea4617cb538
Ben Romdhane Aymen <aymen.bromdhane@yahoo.fr>
2026-02-04 14:57:00 +0100

    Initial Commit

---
+++ ui train/ui_trainApp.swift
@@ -0,0 +1,17 @@
+//
+//  ui_trainApp.swift
+//  ui train
+//
+//  Created by Ben Romdhane on 04/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct ui_trainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
